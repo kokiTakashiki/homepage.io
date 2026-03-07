7b45911e82f92a5448af789efe37f188dd3e5c2c
takedatakashiki <takedatakashiki@takedatakashikinoMacBook-Pro.local>
2026-03-07 19:26:50 +0900

    修正

--- js/works.js
+++ js/works.js
@@ -1,21 +1,21 @@
 $(function () {
   var allList = [
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2025.4-',
       title: '2025年4月 名刺管理アプリEightのiOSアプリ開発チーム',
       descriptionURL: 'workDetail/job/2025-04-eight-ios.html',
       description: 'Sansan株式会社のEight事業部にてiOSアプリの作り直しプロジェクトに参加。',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2024.1-',
       title: '2024年1月 SansanのiOSアプリ開発チーム',
       descriptionURL: 'workDetail/job/2024-01-sansan-ios.html',
       description: 'Sansan株式会社に入社し、営業DX SansanのiOSアプリを開発するチームに参加。',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2023.2-',
       title: '2023年2月 ライブ配信アプリ ミクチャのiOSアプリ開発チーム',
       descriptionURL: 'workDetail/job/2023-02-mixch-ios.html',
@@ -23,7 +23,7 @@ $(function () {
         '株式会社DONUTSに入社し、ライブ配信サービスのミクチャのアプリを開発するチームに参加。',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2022.4-',
       title: '2022年4月 化粧品小売アプリ @cosmeのiOSアプリ開発チーム',
       descriptionURL: 'workDetail/job/2022-04-cosme-ios.html',
@@ -37,7 +37,7 @@ $(function () {
       description: 'VRChat用ワールド。仮装空間SNSプラットフォームのVRChat上で公開。',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2021.7-',
       title: '2021年7月 セコム株式会社様のアプリの運用・保守チーム',
       descriptionURL: 'workDetail/job/2021-07-secom.html',
@@ -45,7 +45,7 @@ $(function () {
         'ココセコムあんしんアプリ・Home Security App・SECURILOCK SMARTの3つのアプリの運用と保守を行うチームに参加。',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2021.3-',
       title: '2021年3月 音楽健康指導士支援アプリのFREE DAMの開発チーム',
       descriptionURL: 'workDetail/job/2021-03-freedam.html',
@@ -60,7 +60,7 @@ $(function () {
         'VRChat用ワールド。<br>16名までがオンラインで楽しむことができるワールドです。<br><br>A world for VRChat, where up to 16 people can be online.',
     },
     {
-      imageFileName: 'work_icon',
+      imageFileName: '',
       life: '2020.1-',
       title: '2020年1月 ThingsBoard 製造業向けデータ可視化Webアプリ開発チーム',
       descriptionURL: 'workDetail/job/2020-01-thingsboard.html',
@@ -126,24 +126,30 @@ $(function () {
       _.each(list, function (line, i) {
         outputHtml += '<div class="product" id="product-' + i + '">';
         outputHtml += '  <h3 class="productTitle">' + line.title + '</h3>';
-        outputHtml +=
-          '  <div class="work-image"><a href="' +
-          line.descriptionURL +
-          '"><img src="../img/' +
-          line.imageFileName +
-          '.jpg" alt="' +
-          line.title +
-          '" /></a></div>';
+        if (line.imageFileName.length > 0) {
+          outputHtml +=
+            '  <div class="work-image"><a href="' +
+            line.descriptionURL +
+            '"><img src="../img/' +
+            line.imageFileName +
+            '.jpg" alt="' +
+            line.title +
+            '" /></a></div>';
+        }
         outputHtml += '  <div class="info">';
         outputHtml += '    <ul></ul>';
-        outputHtml += '    <p class="description">' + line.description + '</p><br />';
+        outputHtml +=
+          '    <p class="description"><a href="' +
+          line.descriptionURL +
+          '">' +
+          line.description +
+          '</a></div>';
         outputHtml += '  </div>';
         outputHtml += '<!--/.product--></div>';
       });
     } else {
       $('.worksToc').empty();
-      outputHtml +=
-        '<div class="noproduct"><p>条件に当てはまるwordを検索できませんでした。</p></div>';
+      outputHtml += '表示できるデータがありません';
     }
 
     $('.productArea').html(outputHtml);
